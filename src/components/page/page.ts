b09317ce56a0985552d1f23d3952353f9d5fc560
y-solb <ysolb99@gmail.com>
2021-08-23 00:02:05 +0900

    chore: 주석 삭제

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -3,9 +3,6 @@ import { BaseComponent } from '../component.js';
 export class PageComponent extends BaseComponent<HTMLUListElement> {
   constructor() {
     super('<ul class="page"> This is PageComponent!</ul>');
-    // this.element = document.createElement('ul');
-    // this.element.setAttribute('class', 'page');
-    // this.element.textContent = 'this is PageComponent';
   }
   attach(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
     super.attachTo(parent, position);
