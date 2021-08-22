3136bbfc87ec07ff5bd61f495ea264613769e6e2
y-solb <ysolb99@gmail.com>
2021-08-23 00:09:48 +0900

    feat: add note, todo, video

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
+import { TodoComponent } from './components/page/item/todo.js';
+import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { PageComponent } from './components/page/page.js';
+import { VideoComponent } from './components/page/item/video.js';
 
 class App {
   private readonly page: PageComponent;
@@ -9,6 +12,15 @@ class App {
 
     const img = new ImageComponent('Image HELLO', 'https://picsum.photos/seed/picsum/200/300');
     img.attachTo(appRoot, 'beforeend');
+
+    const note = new NoteComponent('Here is title', 'And content!');
+    note.attachTo(appRoot, 'beforeend');
+
+    const todo = new TodoComponent('Todo List', 'study typescript');
+    todo.attachTo(appRoot, 'beforeend');
+
+    const video = new VideoComponent('Video title', 'https://www.youtube.com/watch?v=-dowcmrcRYc&t=283s');
+    video.attachTo(appRoot, 'beforeend');
   }
 }
 
